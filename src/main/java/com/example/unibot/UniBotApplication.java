a3a55df97a9bf483a4fcfd6a304e2bde1bfe11f2
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2023-01-24 11:38:12 +0200

    clear UniBotApplication.java

--- src/main/java/com/example/unibot/UniBotApplication.java
+++ src/main/java/com/example/unibot/UniBotApplication.java
@@ -1,26 +1,12 @@
 package com.example.unibot;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniBotApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(UniBotApplication.class, args);
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(System.in));
-
-        String data;
-        while (! (data = reader.readLine()).equals("q")) {
-            if (data.equals("abc")) {
-                System.out.println("def");
-            }
-        }
     }
-
 }
